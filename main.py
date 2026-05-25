1815e2df20a78c919af20b15bf40bf39959178f2
asafkiller12 <augbjkkbf@gmail.com>
2026-05-25 13:23:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ class Query(BaseModel):
 # 🧠 نظام الذاكرة الديناميكية (Google Sheets)
 # ==========================================
 # 🚩 تأكد من وضع رابط الـ CSV الخاص بك هنا
-SHEET_CSV_URL = "ضع_رابط_GOOGLE_SHEET_CSV_هنا"
+SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTo0x3S-adDNu2AukMjxcsRM_MRwh8lC3wqJmyjfm4k9skssdYA-pyb-YaksEvu53d444qPu5JgaHrb/pub?output=csv"
 
 def get_dynamic_context(query: str):
     try:
